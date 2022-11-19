ccd7289f633272e0f13d8b49d32ca578f6785f88
Benjamin Kalish <bkalish@gmail.com>
2022-11-19 16:46:27 -0500

    Removed advanced catalog link and fixed css. Fixes #5.

--- src/php/searchform.php
+++ src/php/searchform.php
@@ -19,13 +19,16 @@ $search_preference = get_search_preference();
 	<label class="screen-reader-text" for="<?php $form_id->echo(); ?>-input">Search for:</label>
 	<input class="search-box" id="<?php $form_id->echo(); ?>-input" type="search" value="<?php echo get_search_query(); ?>" name="s" required>
 	<button class="search-button"><span class="icon-search icon"><span class="screen-reader-text">Search</span></span></button>
-	<div class="search-options">
-		<fieldset>
-			<legend class="screen-reader-text">What to search</legend>
-			<input id="<?php $form_id->echo(); ?>-website" class="search-website" name="search-target" value="website" <?php checked( $search_preference, 'website' ); ?> type="radio"><label class="inline" for="<?php $form_id->echo(); ?>-website">Website</label>
-			<input id="<?php $form_id->echo(); ?>-catalog" class="search-catalog" name="search-target" value="catalog" <?php checked( $search_preference, 'catalog' ); ?> type="radio"><label class="inline" for="<?php $form_id->echo(); ?>-catalog">Catalog</label>
-		</fieldset>
-		<a class="advanced-search-link" href="https://northamptn.cwmars.org/eg/opac/advanced?locg=1">Advanced Catalog Search</a>
-	</div>
+	<fieldset class="search-options">
+		<legend class="screen-reader-text">What to search</legend>
+		<label class="inline" for="<?php $form_id->echo(); ?>-website">
+			<input id="<?php $form_id->echo(); ?>-website" class="search-website" name="search-target" value="website" <?php checked( $search_preference, 'website' ); ?> type="radio">
+			Website
+		</label>
+		<label class="inline" for="<?php $form_id->echo(); ?>-catalog">
+			<input id="<?php $form_id->echo(); ?>-catalog" class="search-catalog" name="search-target" value="catalog" <?php checked( $search_preference, 'catalog' ); ?> type="radio">
+			Catalog
+		</label>
+	</fieldset>
 	<input id="<?php $form_id->echo(); ?>-catalog-scope" class="catalog-scope" type="hidden" name="locg" value="1">
 </form>
