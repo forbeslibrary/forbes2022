eafa7fb1f71b50cabb8a51794dcb2f8038ec0c83
Benjamin Kalish <bkalish@gmail.com>
2022-12-23 14:11:05 -0500

    updated caption size and 404 style

--- src/php/404.php
+++ src/php/404.php
@@ -11,14 +11,13 @@ get_header();
 <main id="main">
 	<h1 class="post-title">Error 404</h1>
 	Oh gosh! We cannot find that page!
-	<img style="display: block; max-height: 2in; margin: 1em 0;"
+	<img id="image404"
 		src="<?php echo esc_url( get_theme_file_uri( '/assets/img/404-image-aliens_from_fifties.jpg' ) ); ?>"
 		alt=""
 	>
 	<p>
 		You can visit our <a <?php echo 'href="' . esc_url( home_url() ) . '" title="Forbes Library Home" rel="home"'; ?>>home page</a> or use the search form below. If you still can't find what you are
-		looking for, please <a href="http://forbeslibrary.org/info/contact/">contact
-		us</a>.
+		looking for, please <a href="http://forbeslibrary.org/info/contact/">contact&nbsp;us</a>.
 	</p>
 	<?php get_search_form(); ?>
 </main>
